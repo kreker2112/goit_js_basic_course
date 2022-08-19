e5bd08ed1afe441c9cd179393b21df45668e334b
kreker2112 <kreker2112@gmail.com>
2022-08-19 14:59:04 +0300

    Update tsk2_1app.js

--- goit-js-hw-02/tsk2_1app.js
+++ goit-js-hw-02/tsk2_1app.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const logItems = function(){
-    // const args = array.from(arguments);
-    for(const arg of arguments){
-        console.log('arg: [i] ', arg[0]);
+const logItems = function(array){
+    console.log(arguments);
+    for (let i = 0; i < arguments.length; i += 1) {
+        console.log(arguments[0]);
+       
     }
 }
 
 logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
 
-// hello
