f9e6480d82cff6de0665d041b16a7939ae801f72
kreker2112 <kreker2112@gmail.com>
2022-08-20 17:19:56 +0300

    Task 4 done

--- goit-js-hw-02/tsk2_3app.js
+++ goit-js-hw-02/tsk2_3app.js
@@ -4,7 +4,7 @@
 
 // const findLongestWord = function (string) {
 //   const wordsArray = string.split(" ");
-//   console.log(wordsArray);
+//   console.log("Массив, из которого надо выбрать самое длинное слово: ", wordsArray);
 //   let longestWord = wordsArray[0];
 //   //   console.log(longestWord);
 //   for (let i = 1; i < wordsArray.length; i += 1) {
@@ -19,17 +19,17 @@
 // =============================================================================
 // Вариант 2
 
-const findLongestWord = function (string) {
-  const wordsArray = string.split(" ");
-  console.log("Массив, из которого надо выбрать: ", wordsArray);
-  let longestWord = wordsArray[0];
-    for (let word of wordsArray) {
-    if (word.length > longestWord.length) {
-      longestWord = word;
-    }
-  }
-   return longestWord;
-};
+// const findLongestWord = function (string) {
+//   const wordsArray = string.split(" ");
+//   console.log("Массив, из которого надо выбрать самое длинное слово: ", wordsArray);
+//   let longestWord = wordsArray[0];
+//     for (let word of wordsArray) {
+//     if (word.length > longestWord.length) {
+//       longestWord = word;
+//     }
+//   }
+//    return longestWord;
+// };
 
 // Напиши функцию findLongestWord(string), которая принимает параметром произвольную строку
 // (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
@@ -41,8 +41,8 @@ const findLongestWord = function (string) {
 // /*
 //  * Вызовы функции для проверки работоспособности твоей реализации.
 //  */
-console.log('Самое длинное слово: ', findLongestWord("The quick brown fox jumped over the lazy dog")); // 'jumped'
+// console.log('Самое длинное слово: ', findLongestWord("The quick brown fox jumped over the lazy dog")); // 'jumped'
 
-console.log('Самое длинное слово: ', findLongestWord("Google do a roll")); // 'Google'
+// console.log('Самое длинное слово: ', findLongestWord("Google do a roll")); // 'Google'
 
-console.log('Самое длинное слово: ', findLongestWord("May the force be with you")); // 'force'
+// console.log('Самое длинное слово: ', findLongestWord("May the force be with you")); // 'force'
