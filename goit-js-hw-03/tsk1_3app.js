b3761f19a0ac5a6c6758855b20f67ea9f497287d
kreker2112 <kreker2112@gmail.com>
2022-08-26 13:43:47 +0300

    task 1_3 done

--- goit-js-hw-03/tsk1_3app.js
+++ goit-js-hw-03/tsk1_3app.js
@@ -1,2 +1,39 @@
-'use strict';
+"use strict";
 
+// Напиши скрипт, который, для объекта user, последовательно:
+
+// добавляет поле mood со значением 'happy'
+// заменяет значение hobby на 'skydiving'
+// заменяет значение premium на false
+// выводит содержимое объекта user в формате ключ:значение используя Object.keys() и for...of
+
+const user = {
+  name: "Mango",
+  age: 20,
+  hobby: "html",
+  premium: true,
+};
+
+console.table(user);
+
+const key = "mood";
+
+const value = "happy";
+
+user[key] = value;
+
+console.table(user);
+
+user['hobby'] = 'skydiving';
+
+user['premium'] = false;
+
+console.table(user);
+
+const keys = Object.keys(user);
+
+console.table(keys);
+
+for(const propertie of keys){
+  console.log(`${propertie}: `, user[propertie]);
+}
