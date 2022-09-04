e318b80efe7964ee9bbf9c64859c16b88b6c317f
kreker2112 <kreker2112@gmail.com>
2022-09-04 17:46:19 +0300

    hw_04_done

--- objectapp.js
+++ objectapp.js
@@ -1,17 +1,17 @@
 "use strict";
 
-const book = {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    genres: ["historical prose", "adventure"],
-    isPublic: true,
-    rating: 8.38,
-  };
+// const book = {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["historical prose", "adventure"],
+//     isPublic: true,
+//     rating: 8.38,
+//   };
   
-  book.rating = 9;
-  book.isPublic = false;
-  book.genres.push("драма");
+//   book.rating = 9;
+//   book.isPublic = false;
+//   book.genres.push("драма");
   
-  console.log(book.rating); // 9
-  console.log(book.isPublic); // false
-  console.log(book.genres); // ['historical prose', 'adventures', 'драма']
+//   console.log(book.rating); // 9
+//   console.log(book.isPublic); // false
+//   console.log(book.genres); // ['historical prose', 'adventures', 'драма']
