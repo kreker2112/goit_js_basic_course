cf8985826444d04f8048197d3809ecf38f65003a
kreker2112 <kreker2112@gmail.com>
2023-03-22 14:00:08 +0200

    task 6 update

--- goit-js-hw-06/tsk1_6app.js
+++ goit-js-hw-06/tsk1_6app.js
@@ -164,9 +164,14 @@ console.log(calculateTotalBalance(users)); // 20916
 
 // Массив имен всех пользователей у которых есть друг с указанным именем.
 
-const getUsersWithFriend = (users, friendName) => {
-    
-};
+const getUsersWithFriend = (users, friendName) =>
+  users
+    .filter((user) => Object.values(user.friends).includes(friendName))
+    .map((user) => user.name);
 
 console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
+
+// Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
+
+
