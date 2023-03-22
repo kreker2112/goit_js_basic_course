c9f9d878c87c51f994fc359b8e9cdbbfb64895d9
kreker2112 <kreker2112@gmail.com>
2023-03-23 01:23:04 +0200

    task 6 update

--- goit-js-hw-06/tsk1_6app.js
+++ goit-js-hw-06/tsk1_6app.js
@@ -184,9 +184,27 @@ console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sher
 
 // Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
 
-const getNamesSortedByFriendsCount = (users) => {
-
-};
+const getNamesSortedByFriendsCount = (users) =>
+  users
+    .sort((previuosUser, CurrentUser) => {
+      if (previuosUser.friends.length < CurrentUser.friends.length) return -1;
+      if (previuosUser.friends.length > CurrentUser.friends.length) return 1;
+      return 0;
+    })
+    .map((user) => user.name);
 
 console.log(getNamesSortedByFriendsCount(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
+// Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
+
+const getSortedUniqueSkills = (users) => {
+  // let usersSkills = [];
+  return users.reduce((allSkills, user) => {
+    allSkills.push(...user.skills);
+    return allSkills;
+  }, []);
+};
+
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
