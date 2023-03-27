c6da9abbcb6948afe04af13159b7cdf4b189ac4b
kreker2112 <kreker2112@gmail.com>
2023-03-27 18:29:20 +0300

    task 07-02 create

--- goit-js-hw-07/task-01.js
+++ goit-js-hw-07/task-01.js
@@ -1,6 +1,52 @@
-'use strict';
+// В HTML есть список категорий ul#categories.
 
-const allCategories = [...document.querySelectorAll('.item')];
+// <ul id="categories">
+//   <li class="item">
+//     <h2>Животные</h2>
+
+//     <ul>
+//       <li>Кот</li>
+//       <li>Хомяк</li>
+//       <li>Лошадь</li>
+//       <li>Попугай</li>
+//     </ul>
+//   </li>
+//   <li class="item">
+//     <h2>Продукты</h2>
+
+//     <ul>
+//       <li>Хлеб</li>
+//       <li>Петрушка</li>
+//       <li>Творог</li>
+//     </ul>
+//   </li>
+//   <li class="item">
+//     <h2>Технологии</h2>
+
+//     <ul>
+//       <li>HTML</li>
+//       <li>CSS</li>
+//       <li>JavaScript</li>
+//       <li>React</li>
+//       <li>Node</li>
+//     </ul>
+//   </li>
+// </ul>
+
+// Напиши скрипт, который выполнит следующие операции.
+
+// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item. Получится 'В списке 3 категории.'.
+
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега h2) и количество элементов в категории (всех вложенных в него элементов li).
+
+// Например для первой категории получится:
+
+// Категория: Животные
+// Количество элементов: 4
+
+"use strict";
+
+const allCategories = [...document.querySelectorAll(".item")];
 
 console.log(allCategories);
 
@@ -8,20 +54,12 @@ const allCategoriesLength = allCategories.length;
 
 console.log(`В списке ${allCategoriesLength} категории`);
 
-const categoriesTextContent = allCategories.forEach(element => {
-    
-    const title = element.querySelector('h2').innerHTML;
+const categoriesTextContent = allCategories.forEach((element) => {
+  const title = element.querySelector("h2").innerHTML;
 
-    const categoryLength = element.querySelectorAll('li').length
+  const categoryLength = element.querySelectorAll("li").length;
 
-    console.log(` - Категория:  ${title}`);
+  console.log(` - Категория:  ${title}`);
 
-    console.log(` - Количество элементов: ${categoryLength}`);
-   
+  console.log(` - Количество элементов: ${categoryLength}`);
 });
-
-
-
-
-  
-
