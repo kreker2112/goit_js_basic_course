c54c9a1db7d9f8dc8b14b33c6d208d92cd9a8c9b
kreker2112 <kreker2112@gmail.com>
2023-04-23 01:42:35 +0300

    task 7-4 done

--- goit-js-hw-07/task-04.js
+++ goit-js-hw-07/task-04.js
@@ -1,7 +1,46 @@
-'use strict';
+"use strict";
 
 // Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать значение счетчика на 1.
 
 // Создай переменную counterValue в которой будет хранится текущее значение счетчика.
 // Создай функции increment и decrement для увеличения и уменьшения значения счетчика
 // Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
+
+class Counter {
+  constructor(initialValue = 0, step = 1) {
+    this.value = initialValue;
+    this.step = step;
+  }
+  increment() {
+    this.value += this.step;
+  }
+  decrement() {
+    this.value -= this.step;
+  }
+
+  getCounterValue() {
+    counterValueField.textContent = this.value;
+  }
+}
+
+const counter = new Counter();
+
+const incrementButton = document.querySelector(
+  'button[data-action="increment"]'
+);
+
+const decrementButton = document.querySelector(
+  'button[data-action="decrement"]'
+);
+
+const counterValueField = document.querySelector("span");
+
+incrementButton.addEventListener("click", () => {
+  counter.increment();
+  counter.getCounterValue();
+});
+
+decrementButton.addEventListener("click", () => {
+  counter.decrement();
+  counter.getCounterValue();
+});
