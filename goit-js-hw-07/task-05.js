275ec81e8ee6ec946dc4270627c58542e0763218
kreker2112 <kreker2112@gmail.com>
2023-04-24 02:33:18 +0300

    task 7-5 update

--- goit-js-hw-07/task-05.js
+++ goit-js-hw-07/task-05.js
@@ -1,8 +1,26 @@
-'use strict';
+"use strict";
 
-// Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), 
-// подставляет его текущее значение в span#name-output. Если инпут пустой, 
+// Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input),
+// подставляет его текущее значение в span#name-output. Если инпут пустой,
 // в спане должна отображаться строка 'незнакомец'.
 
 // <input type="text" placeholder="Ваше имя?" id="name-input" />
 // <h1>Привет, <span id="name-output">незнакомец</span>!</h1>
+
+const nameInput = document.querySelector("input");
+
+const nameOutput = document.querySelector("span");
+
+function spanUpdate() {
+  nameOutput.textContent = nameInput.value;
+}
+
+console.log(nameInput);
+
+console.log(nameOutput);
+
+console.log(nameInput.placeholder);
+
+console.log(nameOutput.textContent);
+
+nameInput.addEventListener("input", spanUpdate);
