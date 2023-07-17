ea81acdd73b8895aaa8d6469ae12f5f91b447583
kreker2112 <kreker2112@gmail.com>
2023-07-18 01:04:20 +0300

    Promise lesson update

--- Promise-API-Lesson/src/js/cart-example/cart-items.js
+++ Promise-API-Lesson/src/js/cart-example/cart-items.js
@@ -3,4 +3,10 @@ import uniqid from "uniqid";
 export const items = [
   { id: uniqid("building-", "-misc"), name: "Монорельса" },
   { id: uniqid("goods-", "-new"), name: "Ховерборд" },
+  { id: uniqid("goods-", "-new"), name: "Железные жупи" },
+  { id: uniqid("goods-", "-new"), name: "Сканер" },
+  { id: uniqid("goods-", "-new"), name: "Телекинетический захват" },
+  { id: uniqid("goods-", "-new"), name: "Рогостовый корпус" },
+  { id: uniqid("goods-", "-new"), name: "Ультракинетичкий радар" },
+  { id: uniqid("goods-", "-new"), name: "Кварковая бомба" },
 ];
--- Promise-API-Lesson/src/js/index.js
+++ Promise-API-Lesson/src/js/index.js
@@ -9,18 +9,27 @@ import "./../css/cart-items.css";
 // Импорт ссылок на ДОМ-элементы из отдельного файла======================================
 
 import { refs } from "./promise-intro/refs.js";
+// import promiseIntroTemplate from "./promise-intro/promise-intro-tmplt.hbs";
 
 // Импорт промиса из отдельного файла=====================================================
 
 import { promise } from "./promise-intro/promise-example.js";
 
-// Примеры работы с промисами из файла promise-template.js================================
+// const promiseIntroRef = document.querySelector(".promise__intro__list");
+
+// promiseIntroRef.insertAdjacentHTML("beforeend", promiseIntroTemplate());
+
+// Примеры работы с промисами из файла promise-example.js================================
 
 refs.beforePromise.textContent = "before promise";
+
+// Определение функции успеха, которая будет передана в метод then.
 
 const onSuccess = (message) => {
   console.log(message);
 };
+
+// Определение функции ошибки, которая будет передана в метод catch.
 
 const onError = (error) => {
   console.error(`error: ${error}`);
@@ -34,6 +43,18 @@ refs.afterPromise.textContent = "after promise";
 
 import { cart } from "./cart-example/cartAsync.js";
 import { items } from "./cart-example/cart-items.js";
+import cartItemsTemplate from "./cart-example/cart-items-tmplt.hbs";
+const cartItemsRef = document.querySelector(".table__body");
+
+function buildCartItems() {
+  const markup = items.map((item) => cartItemsTemplate(item)).join("");
+  cartItemsRef.insertAdjacentHTML("beforeend", markup);
+}
+
+function buildCartItem(item) {
+  const markup = cartItemsTemplate(item);
+  cartItemsRef.insertAdjacentHTML("beforeend", markup);
+}
 
 // Примеры работы с методами промисов из файла cartAsync.js=======================================
 
@@ -56,8 +77,10 @@ const removedItem = cart
   });
 
 function updateUIAfterAdd(item) {
-  console.log(`Updating with item ${item.name} and id ${item.id}`);
+  buildCartItem(item);
 }
+
+console.log("items: ", items);
 
 function updateUIAfterRemove(id) {
   console.log(`Removing with item id ${id}`);
@@ -66,6 +89,7 @@ function updateUIAfterRemove(id) {
 // Создание и вызов синхронной функции, которая возвращает мгновенный промис без таймаута==============================
 
 function updateUIAfterUpdate(id) {
+  buildCartItems(items);
   console.log(
     `Сначала выполняется эта функция мгновенно после всего синхронного кода: Updating with item id ${id}`
   );
@@ -78,87 +102,87 @@ cart
     console.error(error);
   });
 
-console.log("items: ", items);
+// console.log("items: ", items);
 
 // Импорт массивов, объектов и функций из папки racetrack================================================
 
-import { race } from "./racetrack/racetrack.js";
-import { horse } from "./racetrack/horses.js";
-import { notifyWhenHorseCrossesFinishLine } from "./racetrack/notify-when-horse-finished.js";
-import { notifyWhenHorseCrashed } from "./racetrack/notify-when-horse-crashed.js";
-import { notifyWhenRaceFinished } from "./racetrack/notifyWhenRaceFinished.js";
-import { horses } from "./racetrack/horses.js";
-import { notifyOnWinner } from "./racetrack/notifyOnWinner.js";
-console.table("Horses: ", horses);
+// import { race } from "./racetrack/racetrack.js";
+// import { horse } from "./racetrack/horses.js";
+// import { notifyWhenHorseCrossesFinishLine } from "./racetrack/notify-when-horse-finished.js";
+// import { notifyWhenHorseCrashed } from "./racetrack/notify-when-horse-crashed.js";
+// import { notifyWhenRaceFinished } from "./racetrack/notifyWhenRaceFinished.js";
+// import { horses } from "./racetrack/horses.js";
+// import { notifyOnWinner } from "./racetrack/notifyOnWinner.js";
+// console.table("Horses: ", horses);
 
 // Примеры работы с методами промисов из файла racetrack.js=======================================
 
-race(horse)
-  .then(notifyWhenHorseCrossesFinishLine)
-  .catch(notifyWhenHorseCrashed);
+// race(horse)
+//   .then(notifyWhenHorseCrossesFinishLine)
+//   .catch(notifyWhenHorseCrashed);
 
 // Примеры работы с методами промисов all и race из файла racetrack.js=======================================
 
 // Создаем массив промисов, который будет передан в методы all и race.
 
-const promises = horses.map((horse) => race(horse));
+// const promises = horses.map((horse) => race(horse));
 
 // Метод all принимает массив промисов и возвращает новый промис, который завершится, когда завершатся все промисы в массиве.
 
-notifyWhenRaceFinished(promises);
+// notifyWhenRaceFinished(promises);
 
 // Метод race принимает массив промисов и возвращает новый промис, который завершится, когда завершится первый промис в массиве.
 
-notifyOnWinner(promises);
+// notifyOnWinner(promises);
 
 // Импорт примера цепного промиса из отдельного файла===============================================
 
-import { chainedPromiseExample } from "./Chained_promise/chained-promise.js";
+// import { chainedPromiseExample } from "./Chained_promise/chained-promise.js";
 
 // Примеры работы с цепными промисами из файла chained-promise.js=======================================
 
-chainedPromiseExample
-  .then((value1) => {
-    console.log("resolve 1 - ", value1);
-    return value1 * 2;
-  })
-  .then((value2) => {
-    console.log("resolve 2 - ", value2);
+// chainedPromiseExample
+//   .then((value1) => {
+//     console.log("resolve 1 - ", value1);
+//     return value1 * 2;
+//   })
+//   .then((value2) => {
+//     console.log("resolve 2 - ", value2);
 
-    return value2 * 3;
-  })
-  .then((value3) => {
-    console.log("resolve 3 - ", value3);
-    return value3 * 4;
-  })
-  .then((value4) => {
-    console.log("resolve 4 - ", value4);
-    throw new Error("Error in resolve 4");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+//     return value2 * 3;
+//   })
+//   .then((value3) => {
+//     console.log("resolve 3 - ", value3);
+//     return value3 * 4;
+//   })
+//   .then((value4) => {
+//     console.log("resolve 4 - ", value4);
+//     throw new Error("Error in resolve 4");
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
 
 // Примеры работы с бэкендом на примере Fetch API из файла fetchFromSwapi.js=======================================
 
-fetch("https://swapi.dev/api/planets")
-  .then((response) => response.json())
-  .then((data) => console.log(data.results))
-  .catch((error) => console.error(error));
+// fetch("https://swapi.dev/api/planets")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data.results))
+//   .catch((error) => console.error(error));
 
 // Импорт файла геолокации из отдельного файла===============================================
 
-import { getGeoPosition } from "./geo/geo.js";
+// import { getGeoPosition } from "./geo/geo.js";
 
 // Примеры работы с геолокацией из файла geo.js=======================================
 
-getGeoPosition()
-  .then((position) => {
-    console.log("Geo position: ", position);
-  })
-  .catch((error) => {
-    console.error(err.message);
-  });
+// getGeoPosition()
+//   .then((position) => {
+//     console.log("Geo position: ", position);
+//   })
+//   .catch((error) => {
+//     console.error(err.message);
+//   });
 
 // RAF
 // window.requestAnimationFrame()
