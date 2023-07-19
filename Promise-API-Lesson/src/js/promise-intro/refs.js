2904911e7ef09969020c8089a97e50e90cb46ffb
kreker2112 <kreker2112@gmail.com>
2023-07-20 01:17:03 +0300

    promise lesson update

--- Promise-API-Lesson/src/js/promise-intro/refs.js
+++ Promise-API-Lesson/src/js/promise-intro/refs.js
@@ -1,7 +1,8 @@
 "use strict";
 
 export const refs = {
-  promiseApiOutput: document.getElementById("output"),
-  beforePromise: document.getElementById("before_promise_output"),
-  afterPromise: document.getElementById("after_promise_output"),
+  promiseLesson: document.querySelector(".promise__lesson"),
+  // promiseApiOutput: document.getElementById("output"),
+  // beforePromise: document.getElementById("before_promise_output"),
+  // afterPromise: document.getElementById("after_promise_output"),
 };
