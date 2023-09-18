fa7168f378ccce25e4f31d9af000af42ddf6607f
kreker2112 <kreker2112@gmail.com>
2023-09-18 21:05:34 +0300

    Updates

--- main.bundle.js
+++ main.bundle.js
@@ -2,7 +2,7 @@
 /******/ 	"use strict";
 var __webpack_exports__ = {};
 
-;// CONCATENATED MODULE: ./gallery-items.js
+;// CONCATENATED MODULE: ./js/gallery-items.js
 var galleryItems = [{
   preview: "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg",
   original: "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg",
