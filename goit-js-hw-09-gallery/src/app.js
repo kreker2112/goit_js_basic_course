7cb463285fecbde839d6f43dc2be09ea4b9c5adb
kreker2112 <kreker2112@gmail.com>
2023-09-18 21:04:58 +0300

    hw-9-gallery_update

--- goit-js-hw-09-gallery/src/app.js
+++ goit-js-hw-09-gallery/src/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './js/gallery-items.js';
 
-import './styles.css';
+import './css/styles.css';
 
 let index;
 
