05411f1dbb693838038e75c150b189fb2966f43b
kreker2112 <kreker2112@gmail.com>
2023-09-25 02:57:49 +0300

    hw-11-timer-update

--- goit-js-hw-11/timer/index.js
+++ goit-js-hw-11/timer/index.js
@@ -14,25 +14,111 @@
  * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
  * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
  */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+// const days = Math.floor(time / (1000 * 60 * 60 * 24));
 
 /*
  * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
  * остатка % и делим его на количество миллисекунд в одном часе
  * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
  */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+// const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 
 /*
  * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
  * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
  */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+// const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
 
 /*
  * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
  * миллисекунд в одной секунде (1000)
  */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+// const secs = Math.floor((time % (1000 * 60)) / 1000);
 
 // ======================================================================================================
+
+class CountdownTimer {
+  // Объект ссылок на элементы интерфейса таймера (публичный)
+  refs = {
+    days: document.querySelector('[data-value="days"]'),
+    hours: document.querySelector('[data-value="hours"]'),
+    mins: document.querySelector('[data-value="mins"]'),
+    secs: document.querySelector('[data-value="secs"]'),
+  };
+  //   Объявление конструктора класса таймера (публичный)
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+  //   Метод запуска таймера (публичный)
+  start() {
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = this.targetDate - currentTime;
+      const time = this.getTimeComponents(deltaTime);
+      this.updateClockface(time);
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.start);
+  }
+  //   Методы для работы с таймером (приватные)
+  //   Метод добавления нуля к числу меньше 10 (приватный)
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+  //   Метод получения компонентов времени (приватный)
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    return { days, hours, mins, secs };
+  }
+  //   Метод обновления интерфейса таймера (приватный)
+  updateClockface({ days, hours, mins, secs }) {
+    this.refs.days.textContent = `${days}`;
+    this.refs.hours.textContent = `${hours}`;
+    this.refs.mins.textContent = `${mins}`;
+    this.refs.secs.textContent = `${secs}`;
+  }
+}
+
+// ======================================================================================================
+
+// Создание экземпляра класса таймера с передачей параметров (публичный)
+// const timer = new CountdownTimer({
+//   selector: "#timer-1",
+//   targetDate: new Date("Sep 26, 2023"),
+// });
+
+// Запуск таймера (публичный)
+
+// timer.start();
+
+// ======================================================================================================
+
+const dateChoose = document.querySelector("#date");
+
+const startBtn = document.querySelector(".start-button");
+
+const stopBtn = document.querySelector(".stop-button");
+
+console.dir(dateChoose);
+
+startBtn.addEventListener("click", () => {
+  const date = dateChoose.value;
+  console.log(date);
+  const timer = new CountdownTimer({
+    selector: "#timer-1",
+    targetDate: new Date(date),
+  });
+  timer.start();
+});
+
+stopBtn.addEventListener("click", () => {
+  timer.stop();
+});
