6b63d4f6d35c373f298b0b35dc4fbbc9476bac71
kreker2112 <kreker2112@gmail.com>
2023-10-03 02:16:37 +0300

    hw-13_update

--- goit-js-hw-13-image-finder/src/index.js
+++ goit-js-hw-13-image-finder/src/index.js
@@ -1,41 +1,148 @@
 "use strict";
 
+// Импорт стилей:
+
 import "./css/styles.css";
+
+// Импорт библиотеки для уведомлений:
 
 import Notiflix from "notiflix";
 
+// Импорт функции для получения изображений:
+
 import { getImages } from "./js/get-images.js";
+
+// Импорт шаблона карточки изображения:
 
 import imageCard from "./templates/photo-card.hbs";
 
+// Импорт ссылок на элементы:
+
 import { refs } from "./js/refs.js";
+
+// Функция рендера карточек изображений:
+function renderImageCard(imagesArr) {
+  const markup = imagesArr.map((image) => imageCard(image)).join("");
+  refs.gallery.insertAdjacentHTML("beforeend", markup);
+}
+
+// Объявление переменных:
+
+let pageNumber = 1;
+let currentHits = 0;
+let searchQuery = "";
+
+// Функция поиска изображений:
 
 function onSearch(e) {
   e.preventDefault();
-  const input = document.querySelector(".search-form__input");
-  console.log(input.value);
-  getImages(input.value)
+
+  // Запись значения инпута в переменную:
+  searchQuery = e.currentTarget.searchQuery.value;
+
+  // Запись количества страниц в переменную:
+  pageNumber = 1;
+
+  // Проверка на пустой запрос:
+  if (searchQuery === "") {
+    return Notiflix.Notify.failure("Please enter a search query!");
+  }
+
+  // Запись ответа от сервера в переменную:
+  const imagesResponse = getImages(searchQuery, pageNumber);
+  // Вызов цепочки промисов для получения изображений:
+  imagesResponse
     .then((images) => {
+      // Запись массива изображений из ответа от сервера в переменную:
       const imagesArr = images.data.hits;
-      console.log(imagesArr);
-      if (imagesArr.length === 0) {
-        alertNoMatches();
+      console.log("imagesArr: ", imagesArr);
+
+      // Запись количества найденных изображений в переменную:
+      currentHits = images.data.hits.length;
+      console.log("currentHits: ", currentHits);
+
+      // Запись общего количества найденных изображений в переменную:
+      const totalHits = images.data.totalHits;
+      console.log("totalHits: ", totalHits);
+
+      // Проверка на наличие совпадений:
+      if (currentHits === 0) {
+        return alertNoMatches();
       }
+
+      // Очистка галереи:
       refs.gallery.innerHTML = "";
-      refs.gallery.insertAdjacentHTML("beforeend", renderImageCard(imagesArr));
+
+      // Рендер карточек изображений:
+      renderImageCard(imagesArr);
+
+      // Добавление кнопки "Загрузить еще":
+      addMoreButton(totalHits);
+
+      // Подписка на событие клика по кнопке "Загрузить еще":
+      refs.loadMoreBtn.addEventListener("click", addMoreImagesOnClick);
+      // Проверка на последнюю страницу:
+      if (totalHits <= pageNumber * 40) {
+        refs.loadMoreBtn.classList.remove("is-open");
+        warningNoMatches();
+      }
     })
     .catch(console.warn);
 }
 
-function renderImageCard(imagesArr) {
-  const markup = imagesArr.map((image) => imageCard(image)).join("");
-  return markup;
-}
-
+// Функция уведомления об отсутствии совпадений:
 function alertNoMatches() {
   Notiflix.Notify.failure(
     "Sorry, there are no images matching your search query. Please try again."
   );
+}
+
+function warningNoMatches() {
+  Notiflix.Notify.warning(
+    "We're sorry, but you've reached the end of search results."
+  );
+}
+
+// Функция добавления кнопки "Загрузить еще":
+function addMoreButton(totalHits) {
+  if (totalHits < 40) {
+    return;
+  }
+  refs.loadMoreBtn.classList.add("is-open");
+}
+
+// Функция добавления изображений при клике на кнопку "Загрузить еще":
+function addMoreImagesOnClick() {
+  // Увеличение номера страницы на 1 при клике на кнопку "Загрузить еще":
+  pageNumber += 1;
+  // Запись ответа от сервера в переменную:
+  const imagesResponse = getImages(searchQuery, pageNumber);
+  // Вызов цепочки промисов для получения изображений:
+  imagesResponse
+    .then((images) => {
+      // Запись массива изображений из ответа от сервера в переменную:
+      const imagesArr = images.data.hits;
+      // Запись количества найденных изображений в переменную:
+      const totalHits = images.data.totalHits;
+      // Проверка на наличие совпадений:
+      if (currentHits === 0) {
+        alertNoMatches();
+        return;
+      }
+      // Рендер карточек изображений:
+      renderImageCard(imagesArr);
+      // Добавление скролла при клике на кнопку "Загрузить еще":
+      const { height: cardHeight } = document
+        .querySelector(".gallery")
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 2.5,
+        behavior: "smooth",
+      });
+    })
+    // Проверка на последнюю страницу:
+    .catch(console.warn);
 }
 
 refs.searchForm.addEventListener("submit", onSearch);
--- goit-js-hw-13-image-finder/src/js/get-images.js
+++ goit-js-hw-13-image-finder/src/js/get-images.js
@@ -1,25 +1,15 @@
 "use strict";
 
-// https://pixabay.com/api/?key=39315149-fad4768c6404d095435b55e12&q=yellow+flowers&image_type=photo&orientation=horizontal&page=1&per_page=12
-
 import axios from "axios";
 
-axios.defaults.baseURL = "https://pixabay.com/";
+axios.defaults.baseURL = "https://pixabay.com/api/";
 
-const key = "39315149-fad4768c6404d095435b55e12";
-
-const imageType = `&image_type=photo`;
-
-const orientation = `&orientation=horizontal`;
-
-const page = "&page=1";
-
-const perPage = "&per_page=40";
-
-export async function getImages(searchInput) {
-  const imageLoad = await axios.get(
-    `api/?key=${key}&q=${searchInput}${imageType}${orientation}${page}${perPage}`
+export async function getImages(searchInput, page) {
+  const key = "39315149-fad4768c6404d095435b55e12";
+  const imageType = `photo`;
+  const orientation = `horizontal`;
+  const perPage = "40";
+  return await axios.get(
+    `?key=${key}&q=${searchInput}&image_type=${imageType}&orientation=${orientation}&page=${page}&per_page=${perPage}`
   );
-
-  return imageLoad;
 }
