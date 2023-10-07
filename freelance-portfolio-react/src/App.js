306a615a236ff97bcca1ffeaeb341944a8b9874e
kreker2112 <kreker2112@gmail.com>
2023-10-08 00:59:48 +0300

    react-lesson-update

---
+++ freelance-portfolio-react/src/App.js
@@ -0,0 +1,22 @@
+import "./styles/main.css";
+
+import Navbar from "./components/navbar/Navbar";
+
+import Footer from "./components/footer/Footer";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+
+      {/* <Home /> */}
+      <Projects />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
