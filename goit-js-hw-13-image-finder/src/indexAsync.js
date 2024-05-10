dbb90e0754206e22c0ea5f763b46c5e95a802a87
kreker2112 <kreker2112@gmail.com>
2024-05-10 16:28:27 +0300

    delete comments

--- goit-js-hw-13-image-finder/src/indexAsync.js
+++ goit-js-hw-13-image-finder/src/indexAsync.js
@@ -1,36 +1,12 @@
 "use strict";
-
-// Импорт стилей:
-
 import "./css/styles.css";
-
-// Импорт библиотеки для уведомлений:
-
 import Notiflix from "notiflix";
-
-// Импорт функции для получения изображений:
-
 import { getImages } from "./js/get-images.js";
-
-// Импорт шаблона карточки изображения:
-
 import imageCard from "./templates/photo-card.hbs";
-
-// Импорт ссылок на элементы:
-
 import { refs } from "./js/refs.js";
-
-// Импорт библиотеки для модального окна:
-
 import SimpleLightbox from "simplelightbox";
-
-// Импорт стилей для модального окна:
-
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-// ========================================================================================
-
-// Функция рендера карточек изображений:
 function renderImageCard(imagesArr) {
   const markup = imagesArr.map((image) => imageCard(image)).join("");
   refs.gallery.insertAdjacentHTML("beforeend", markup);
@@ -42,80 +18,59 @@ let lightbox = new SimpleLightbox(".gallery a", {
   captionDelay: 250,
 });
 
-// Объявление переменных:
-
 let pageNumber = 1;
 let currentHits = 0;
 let searchQuery = "";
 
-// Функция поиска изображений:
-
 async function onSearch(e) {
   e.preventDefault();
 
-  // Запись значения инпута в переменную:
   searchQuery = e.currentTarget.searchQuery.value;
 
-  // Запись количества страниц в переменную:
   pageNumber = 1;
 
-  // Проверка на пустой запрос:
   if (searchQuery === "") {
     return Notiflix.Notify.failure("Please enter a search query!");
   }
 
-  // Запись ответа от сервера в переменную:
   const imagesResponse = await getImages(searchQuery, pageNumber);
 
-  // Запись количества найденных изображений в переменную:
   currentHits = imagesResponse.data.hits.length;
   console.log("currentHits: ", currentHits);
 
-  // Запись общего количества найденных изображений в переменную:
   const totalHits = imagesResponse.data.totalHits;
   console.log("totalHits: ", totalHits);
 
-  // Запись массива изображений из ответа от сервера в переменную:
   const imagesArr = imagesResponse.data.hits;
   console.log("imagesArr: ", imagesArr);
 
-  // Добавление кнопки "Загрузить еще":
   addMoreButton(totalHits);
 
   try {
-    // Вызов функции уведомления об успешном поиске:
     successSearch(totalHits);
 
-    // Проверка на наличие совпадений:
     checkNoMatches();
 
-    // Очистка галереи:
     refs.gallery.innerHTML = "";
 
-    // Рендер карточек изображений:
     renderImageCard(imagesArr);
 
-    // Добавление модального окна:
     lightbox.refresh();
 
-    // Подписка на событие клика по кнопке "Загрузить еще":
     refs.loadMoreBtn.addEventListener("click", addMoreImagesOnClick);
 
-    // Проверка на последнюю страницу:
     checkForLastPage(totalHits);
   } catch (error) {
     console.log(error);
   }
 }
 
-// Функция уведомления об успешном поиске:
 function successSearch(totalHits) {
   if (totalHits > 0) {
     Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
   }
 }
 
-// Функция уведомления об отсутствии совпадений:
 function checkNoMatches() {
   if (currentHits === 0) {
     return Notiflix.Notify.failure(
@@ -124,14 +79,12 @@ function checkNoMatches() {
   }
 }
 
-// Функция уведомления о достижении последней страницы:
 function warningNoMatches() {
   Notiflix.Notify.warning(
     "We're sorry, but you've reached the end of search results."
   );
 }
 
-// Функция добавления кнопки "Загрузить еще":
 function addMoreButton(totalHits) {
   if (totalHits < 40) {
     return;
@@ -139,7 +92,6 @@ function addMoreButton(totalHits) {
   refs.loadMoreBtn.classList.add("is-open");
 }
 
-// Функция проверки на последнюю страницу:
 function checkForLastPage(totalHits) {
   if (totalHits <= pageNumber * 40) {
     refs.loadMoreBtn.classList.remove("is-open");
@@ -147,38 +99,26 @@ function checkForLastPage(totalHits) {
   }
 }
 
-// ========================================================================================
-
-// Функция добавления изображений при клике на кнопку "Загрузить еще":
 async function addMoreImagesOnClick() {
-  // Увеличение номера страницы на 1 при клике на кнопку "Загрузить еще":
   pageNumber += 1;
 
-  // Запись ответа от сервера в переменную:
   const imagesResponse = await getImages(searchQuery, pageNumber);
 
-  // Запись массива изображений из ответа от сервера в переменную:
   const imagesArr = imagesResponse.data.hits;
   console.log("currentHits after LoadMore: ", currentHits * pageNumber);
 
-  // Запись количества найденных изображений в переменную:
   const totalHits = imagesResponse.data.totalHits;
   console.log("totalHits after LoadMore: ", totalHits);
 
   try {
-    // Проверка на наличие совпадений:
     checkNoMatches(currentHits);
 
-    // Рендер карточек изображений:
     renderImageCard(imagesArr);
 
-    // Добавление модального окна:
     lightbox.refresh();
 
-    // Проверка на последнюю страницу:
     checkForLastPage(totalHits);
 
-    // Добавление скролла при клике на кнопку "Загрузить еще":
     const { height: cardHeight } = document
       .querySelector(".gallery")
       .firstElementChild.getBoundingClientRect();
@@ -192,8 +132,4 @@ async function addMoreImagesOnClick() {
   }
 }
 
-// Подписка на событие отправки формы поиска:
-
 refs.searchForm.addEventListener("submit", onSearch);
-
-// ========================================================================================
--- goit-js-hw-13-image-finder/src/indexChaining.js
+++ goit-js-hw-13-image-finder/src/indexChaining.js
@@ -1,92 +1,58 @@
 "use strict";
 
-// Импорт стилей:
-
 import "./css/styles.css";
-
-// Импорт библиотеки для уведомлений:
-
 import Notiflix from "notiflix";
-
-// Импорт функции для получения изображений:
-
 import { getImages } from "./js/get-images.js";
-
-// Импорт шаблона карточки изображения:
-
 import imageCard from "./templates/photo-card.hbs";
-
-// Импорт ссылок на элементы:
-
 import { refs } from "./js/refs.js";
 
-// Функция рендера карточек изображений:
 function renderImageCard(imagesArr) {
   const markup = imagesArr.map((image) => imageCard(image)).join("");
   refs.gallery.insertAdjacentHTML("beforeend", markup);
 }
 
-// Объявление переменных:
-
 let pageNumber = 1;
 let currentHits = 0;
 let searchQuery = "";
 
-// Функция поиска изображений:
-
 function onSearch(e) {
   e.preventDefault();
 
-  // Запись значения инпута в переменную:
   searchQuery = e.currentTarget.searchQuery.value;
 
-  // Запись количества страниц в переменную:
   pageNumber = 1;
 
-  // Проверка на пустой запрос:
   if (searchQuery === "") {
     return Notiflix.Notify.failure("Please enter a search query!");
   }
 
-  // Запись ответа от сервера в переменную:
   const imagesResponse = getImages(searchQuery, pageNumber);
 
-  // Вызов цепочки промисов для получения изображений:
   imagesResponse
     .then((images) => {
-      // Запись массива изображений из ответа от сервера в переменную:
       const imagesArr = images.data.hits;
       console.log("imagesArr: ", imagesArr);
 
-      // Запись количества найденных изображений в переменную:
       currentHits = images.data.hits.length;
       console.log("currentHits: ", currentHits);
 
-      // Запись общего количества найденных изображений в переменную:
       const totalHits = images.data.totalHits;
       console.log("totalHits: ", totalHits);
 
-      // Вызов функции уведомления об успешном поиске:
       successSearch(totalHits);
 
-      // Проверка на наличие совпадений:
       if (currentHits === 0) {
         return alertNoMatches();
       }
 
-      // Очистка галереи:
       refs.gallery.innerHTML = "";
 
-      // Рендер карточек изображений:
       renderImageCard(imagesArr);
 
-      // Добавление кнопки "Загрузить еще":
       addMoreButton(totalHits);
 
-      // Подписка на событие клика по кнопке "Загрузить еще":
       refs.loadMoreBtn.addEventListener("click", addMoreImagesOnClick);
 
-      // Проверка на последнюю страницу:
       checkForLastPage(totalHits);
     })
     .catch(console.warn);
@@ -98,21 +64,18 @@ function successSearch(totalHits) {
   }
 }
 
-// Функция уведомления об отсутствии совпадений:
 function alertNoMatches() {
   Notiflix.Notify.failure(
     "Sorry, there are no images matching your search query. Please try again."
   );
 }
 
-// Функция уведомления о достижении последней страницы:
 function warningNoMatches() {
   Notiflix.Notify.warning(
     "We're sorry, but you've reached the end of search results."
   );
 }
 
-// Функция добавления кнопки "Загрузить еще":
 function addMoreButton(totalHits) {
   if (totalHits < 40) {
     return;
@@ -127,39 +90,28 @@ function checkForLastPage(totalHits) {
   }
 }
 
-// ========================================================================================
-
-// Функция добавления изображений при клике на кнопку "Загрузить еще":
 function addMoreImagesOnClick() {
-  // Увеличение номера страницы на 1 при клике на кнопку "Загрузить еще":
   pageNumber += 1;
 
-  // Запись ответа от сервера в переменную:
   const imagesResponse = getImages(searchQuery, pageNumber);
 
-  // Вызов цепочки промисов для получения изображений:
   imagesResponse
     .then((images) => {
-      // Запись массива изображений из ответа от сервера в переменную:
       const imagesArr = images.data.hits;
       console.log("currentHits after LoadMore: ", currentHits * pageNumber);
 
-      // Запись количества найденных изображений в переменную:
       const totalHits = images.data.totalHits;
       console.log("totalHits after LoadMore: ", totalHits);
 
-      // Проверка на наличие совпадений:
       if (currentHits === 0) {
         alertNoMatches();
         return;
       }
-      // Рендер карточек изображений:
+
       renderImageCard(imagesArr);
 
-      // Проверка на последнюю страницу:
       checkForLastPage(totalHits);
 
-      // Добавление скролла при клике на кнопку "Загрузить еще":
       const { height: cardHeight } = document
         .querySelector(".gallery")
         .firstElementChild.getBoundingClientRect();
@@ -169,10 +121,7 @@ function addMoreImagesOnClick() {
         behavior: "smooth",
       });
     })
-    // Отлавливание ошибки:
     .catch(console.warn);
 }
-
-// Подписка на событие отправки формы:
 
 refs.searchForm.addEventListener("submit", onSearch);
